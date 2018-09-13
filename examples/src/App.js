13aab1584c456c78c00dfe488ed214c41c2f8f69
Tom Darneix <tomdar87@outlook.com>
2018-09-13 18:58:48 +0200

    init dev, build and examples projects

---
+++ examples/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { HeatMapDate } from 'react-d3-heatmap'
+class App extends Component {
+  render() {
+    return (
+      <div><HeatMapDate /></div>
+    );
+  }
+}
+
+export default App;
